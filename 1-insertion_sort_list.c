738ceb175bf4863e5b05544ef25ccef25b16522b
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-21 13:04:34 -0700

    task1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,13 +6,15 @@
 void insertion_sort_list(listint_t **list)
 {
 listint_t *crrt;
+listint_t *tmp;
+listint_t *sd;
 if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
 crrt = (*list)->next;
 while (crrt != NULL)
 {
-listint_t *tmp = crrt->next;
-listint_t *sd = crrt->prev;
+tmp = crrt->next;
+sd = crrt->prev;
 while (sd && sd->n > crrt->n)
 {
 sd->next = crrt->next;
