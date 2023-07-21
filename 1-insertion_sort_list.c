0689a6c4f1dd6ad9b6641c83c81472018e4be93f
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-21 11:34:41 -0700

    task1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+* insertion_sort_list - insertion sort
+* @list: list
+*/
+void insertion_sort_list(listint_t **list)
+{
+listint_t *crrt = (*list)->next;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
+while (crrt)
+{
+listint_t *tmp = crrt->next;
+listint_t *sd = crrt->prev;
+while (sd && sd->n > crrt->n)
+{
+sd->next = crrt->next;
+if (crrt->next)
+crrt->next->prev = sd;
+crrt->next = sd;
+crrt->prev = sd->prev;
+sd->prev = crrt;
+if (crrt->prev)
+crrt->prev->next = crrt;
+else
+*list = crrt;
+sd = crrt->prev;
+print_list(*list);
+}
+crrt = tmp;
+}
+}
--- sort.h
+++ sort.h
@@ -21,7 +21,7 @@ struct listint_s *next;
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
