c232bc90c76f1fd32e2d4b973630e0870a2e97b7
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-22 11:29:15 -0700

    task3

--- sort.h
+++ sort.h
@@ -32,4 +32,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void quicsort(int *array, int prem, int dern, size_t size);
+int part_funt(int *array, int prem, int dern, size_t size);
 #endif
